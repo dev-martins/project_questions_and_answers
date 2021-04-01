119977849ef8b8cb250f89dce94c382d2beb7cab
cmartins123 <claudio.martins@gmail.com.br>
2021-04-01 18:19:05 -0300

    instalando e configurando bodyParser

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const { request } = require('express');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 /**
  * Informando ao Express que deve utilizar
  * o motor HTML EJS
  */
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 /**
  * carregando arquivos staticos
  */
 app.use(express.static('public'));
 
+/**
+ * usando o body-parser para analisar 
+ * o corpo das requisições
+ */
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
 app.get('/', (request, response) => {
     response.render('index');
 });
+
+app.get('/questions', (request, response) => {
+    response.render('questions');
+});
+
+app.post('/send_reply', (request,response) => {
+    let description = request.body.description;
+    response.send(`Descrição da pergunta: <strong>${description}</strong>`);
+})
 
 app.listen('8001', () => {
     console.log("Servidor rodando...");
