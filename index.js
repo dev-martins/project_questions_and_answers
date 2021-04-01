13537ad627184e0d355768addf794cc67e1a5793
cmartins123 <claudio.martins@gmail.com.br>
2021-04-01 19:20:30 -0300

    conectando ao banco com sequelize

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const { request } = require('express');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require('./database/conectDatabase');
+
+/**
+ * configurando a conexão com o banco de dados
+ */
+
+connection.authenticate()
+    .then(() => {
+        console.log('Conexão realizada com sucesso');
+    })
+    .catch((erro) => {
+        console.log(erro);
+    })
 
 /**
  * Informando ao Express que deve utilizar
@@ -18,7 +31,7 @@ app.use(express.static('public'));
  * usando o body-parser para analisar 
  * o corpo das requisições
  */
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
@@ -29,7 +42,7 @@ app.get('/questions', (request, response) => {
     response.render('questions');
 });
 
-app.post('/send_reply', (request,response) => {
+app.post('/send_reply', (request, response) => {
     let description = request.body.description;
     response.send(`Descrição da pergunta: <strong>${description}</strong>`);
 })
