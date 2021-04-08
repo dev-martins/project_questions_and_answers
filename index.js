95cacfe16c9eb979fd8915be2aff8e9338760c81
cmartins123 <claudio.martins@gmail.com.br>
2021-04-08 13:05:20 -0300

    criando tables com Sequelize

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/conectDatabase');
-
+const questionsModel = require('./database/question')
 /**
  * configurando a conexão com o banco de dados
  */
