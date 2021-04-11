4b04671c4e847691982b4eb4caea4e18807470be
cmartins123 <claudio.martins@gmail.com.br>
2021-04-11 14:47:52 -0300

    finalisando projeto

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/conectDatabase');
-const Question = require('./database/question');
+const Question = require('./database/Question');
+const Response = require('./database/response');
 /**
  * configurando a conexão com o banco de dados
  */
@@ -34,40 +35,66 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+/**
+ * página index
+ */
 app.get('/', (request, response) => {
 
-    Question.findAll({ raw: true, order:[
-        [
-            'id','DESC'
+    Question.findAll({
+        raw: true, order: [
+            [
+                'id', 'DESC'
+            ]
         ]
-    ]}).then((questions) => {
-        console.log(questions);
-        response.render('index',{
+    }).then((questions) => {
+        response.render('index', {
             questions: questions
         });
     })
 });
 
+/**
+ * busca todas as questões
+ */
 app.get('/questions', (request, response) => {
     response.render('questions');
 });
 
-app.get('/question/:id',(request,response) => {
+/**
+ * busca a questão pelo id
+ */
+app.get('/question/:id', (request, response) => {
 
     Question.findOne({
-        where:{
+        where: {
             id: request.params.id
         }
     }).then(question => {
-        if(question){
-            response.render('single-question',{
-                question:question
-            });
-        }else{
+        if (question) {
+            Response.findAll({
+                where: { questionsId: request.params.id }
+            }).then(responses => {
+                response.render('single-question', {
+                    question: question,
+                    responses: responses
+                });
+            })
+
+        } else {
             response.redirect('/');
         }
     });
 })
+
+app.post('/response', (request, response) => {
+    Response.create({
+        description: request.body.response,
+        questionsId: request.body.responseId
+    }).then(() => {
+        response.redirect(`/question/${request.body.responseId}`);
+    });
+})
+
 
 app.post('/send_reply', (request, response) => {
 
